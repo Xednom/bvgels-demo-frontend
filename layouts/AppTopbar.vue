a28f73e4c87bb1a8f811cbaada03cd784c41ec6e
Xednom <monde.lacanlalay@gmail.com>
2024-08-30 16:45:08 +0800

    updated AppTopbar.vue to add the logout Menu

--- layouts/AppTopbar.vue
+++ layouts/AppTopbar.vue
@@ -2,6 +2,8 @@
 import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
 import { useLayout } from './composables/layout';
 import { useRouter } from 'vue-router';
+import Menu from 'primevue/menu';
+
 const { layoutConfig, onMenuToggle } = useLayout();
 const outsideClickListener = ref(null);
 const topbarMenuActive = ref(false);
@@ -58,6 +60,30 @@ const isOutsideClicked = (event) => {
 
     return !(sidebarEl.isSameNode(event.target) || sidebarEl.contains(event.target) || topbarEl.isSameNode(event.target) || topbarEl.contains(event.target));
 };
+
+const { data, signOut } = useAuth();
+const menu = ref();
+const items = ref([
+    {
+        separator: true
+    },
+    {
+        label: 'Profile',
+        items: [
+            {
+                label: 'Logout',
+                icon: 'pi pi-sign-out',
+                shortcut: '⌘+Q'
+            }
+        ]
+    },
+    {
+        separator: true
+    }
+]);
+const toggle = (event) => {
+    menu.value.toggle(event);
+};
 </script>
 
 <template>
@@ -76,18 +102,32 @@ const isOutsideClicked = (event) => {
         </button>
 
         <div class="layout-topbar-menu" :class="topbarMenuClasses">
-            <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
-                <i class="pi pi-calendar"></i>
-                <span>Calendar</span>
-            </button>
-            <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
-                <i class="pi pi-user"></i>
-                <span>Profile</span>
-            </button>
-            <button @click="onSettingsClick()" class="p-link layout-topbar-button">
-                <i class="pi pi-cog"></i>
-                <span>Settings</span>
-            </button>
+            <Button type="button" icon="pi pi-ellipsis-v" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu" />
+            <Menu ref="menu" id="overlay_menu" :model="items" :popup="true">
+                <template #start>
+                    <span class="inline-flex align-items-center gap-1 px-2 py-2">
+                        <span class="text-xl font-semibold">Your Company<span class="text-primary"> Name</span></span>
+                    </span>
+                </template>
+                <template #item="{ item, props }">
+                    <a v-ripple @click="() => signOut({ callbackUrl: '/auth/login' })" class="flex align-items-center" v-bind="props.action">
+                        <span :class="item.icon" />
+                        <span class="ml-2">{{ item.label }}</span>
+                        <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
+                        <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{ item.shortcut }}</span>
+                    </a>
+                </template>
+                <template #end>
+                    <button v-ripple class="relative overflow-hidden w-full p-link flex align-items-center p-2 pl-3 text-color hover:surface-200 border-noround">
+                        <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" class="mr-2" shape="circle" />
+                        <span class="inline-flex flex-column">
+                            <span class="font-bold">Welcome, {{ data.first_name }} {{ data.last_name }}</span>
+                            <span class="text-sm" v-if="data.is_superuser">Admin</span>
+                            <span class="text-sm" v-else>Staff</span>
+                        </span>
+                    </button>
+                </template>
+            </Menu>
         </div>
     </div>
 </template>
